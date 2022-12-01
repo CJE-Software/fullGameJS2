741c7e096cc92a6c384ced2404455eb79998f136
CJ Evans <cje.software.engineer@gmail.com>
2022-12-01 12:18:52 -0600

    added deltaTime handling for accessibility

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-document.addEventListener('load', function() {
-        /** @type {HTMLCanvasElement} */
+     /** @type {HTMLCanvasElement} */
+document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
@@ -10,37 +10,53 @@ document.addEventListener('load', function() {
     class Game {
         constructor() {
             this.enemies = [];
+            this.#addNewEnemy();
+            console.log(this.enemies);
         }
         updtae() {
+            this.enemies.forEach(object => object.update());
 
         }
         draw() {
-
+            this.enemies.forEach(object => object.draw());
         }
         #addNewEnemy() {
-
+            this.enemies.push(new Enemy());
         }
-    }
+    };
 
     //note if a method starts with hash# it is a private class method
 
     class Enemy {
         constructor() {
-
+            this.x = 100;
+            this.y = 100;
+            this.width = 100;
+            this.height = 100;
         }
         update() {
-
+            this.x--;
         }
         draw() {
-
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-    }
+    };
 
-    function animate() {
-        ctx.clearRect(0, 0, canvas,width, canvas.height);
+    const game = new Game();
 
+    let lastTime = 1;
+    function animate(timeStamp) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        //somecode
         requestAnimationFrame(animate);
-    }
+    };
+    animate(0);
 
 
 });
+
+//be sure to change second argument to 'load' instead of 'DOMContentLoaded' as shown below
+//document.addEventListener('DOMContentLoaded', function() { GAME }
+//document.addEventListener('Load', function() {
